8eabe48f63647eb391039fa17d9334a79caf4bb3
Easton <eastonself@gmail.com>
2018-06-04 03:24:01 -0400

    all changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(3000, "0.0.0.0");
 
 app.use(express.static('public'));
 console.log("Server is running!");
