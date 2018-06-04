a99efcebf0169b18da8acc5a8412ff291e087cbc
Easton <eastonself@gmail.com>
2018-06-04 02:56:17 -0400

    all files

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+console.log("Server is running!");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+	console.log("new connection @ " + socket.id);
+
+	socket.on('state', stateMsg);
+
+	function stateMsg(data){
+		console.log(data);
+		socket.broadcast.emit('state', data);
+		//io.sockets.emit('mouse', data);
+	}
+}
